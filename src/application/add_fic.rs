b88549605ed351bce90bd2e7b0f3c95fe9e4c818
Yves Motteux <yves.motteux@akeneo.com>
2026-04-18 20:56:52 +0200

    Adding a fanfic already in DB doesn't erase user data

--- src/application/add_fic.rs
+++ src/application/add_fic.rs
@@ -7,6 +7,12 @@ pub fn add_fanfiction(
     fic_id: u64,
     base_url: &str,
 ) -> Result<String, FicflowError> {
+    match db_ops.get_fanfiction_by_id(fic_id) {
+        Ok(_) => return Err(FicflowError::AlreadyExists { fic_id }),
+        Err(FicflowError::NotFound { .. }) => {}
+        Err(e) => return Err(e),
+    }
+
     let fic = fetcher.fetch_fanfiction(fic_id, base_url)?;
     db_ops.save_fanfiction(&fic)?;
     Ok(fic.title)
